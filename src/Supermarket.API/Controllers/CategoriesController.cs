1419fa4ad17483a93644007ee6154bad8937a0eb
erniker <josepablomedinagrande@gmail.com>
2019-07-14 20:03:54 +0200

    Validating the Request Body Using the Model State

--- src/Supermarket.API/Controllers/CategoriesController.cs
+++ src/Supermarket.API/Controllers/CategoriesController.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Services;
+using Supermarket.API.Extensions;
+using Supermarket.API.Resources;
 
 namespace Supermarket.API.Controllers
 {
@@ -24,9 +26,9 @@ namespace Supermarket.API.Controllers
 
         [HttpPost]
         public async Task<IActionResult> PostAsync([FromBody] SaveCategoryResource resource)
-
         {
-
-        }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+        } //Estos errores dan en el punto 9
     }
 }
